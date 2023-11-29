9c93a59517e50d1ceff69877aff48a43ee8edd0d
Sanjayan <sanjayanganesan@gmail.com>
2023-11-29 11:32:49 +0530

    installed next js and creating layout

---
+++ dil-foods-commerce/app/Component/Navbar/navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className="w-full h-20 bg-black flex justify-between">
+      <div className="w-20 h-full mx-5">
+        <img src="https://dilfoods.in/wp-content/uploads/2023/04/Dil-Foods-new-logo.png" />
+      </div>
+      <div className="flex items-center w-20 justify-between me-6"> 
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ dil-foods-commerce/app/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+    <div>
+      Dil Foods
+    </div>
+  )
+}
