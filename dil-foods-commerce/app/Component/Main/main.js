adf682a71fbbb76fc0ac8d6731f4f8cf9a068d9e
Sanjayan <sanjayanganesan@gmail.com>
2023-11-29 22:15:24 +0530

    added the modals for cart

--- dil-foods-commerce/app/Component/Main/main.js
+++ dil-foods-commerce/app/Component/Main/main.js
@@ -1,10 +1,13 @@
 'use client'
 import React, { useState, useEffect } from "react";
 import Modal from "./modal";
+import CartModal from "./cartmodal";
 
 function Main() {
   const [modalOpen, setModalOpen] = useState(false);
   const [selectedProduct, setSelectedProduct] = useState(null);
+  const [modalCartOpen, setModalCartOpen] = useState(false);
+  const [selectedProductCart, setSelectedProductCart] = useState(null);
   const [records, setRecords] = useState([]);
   const [sortingOption, setSortingOption] = useState("Recommended");
   const [selectedCategory, setSelectedCategory] = useState("all");
@@ -39,6 +42,15 @@ function Main() {
   const handelOpenModalProduct = (product) => {
     setSelectedProduct(product);
     setModalOpen(true);
+  };
+
+  const handelOpenModalCart = (product) => {
+    setSelectedProductCart(product);
+    setModalCartOpen(true);
+  };
+
+  const handelCloseModal = () => {
+    setModalCartOpen(false);
   };
 
   const closeModal = () => {
@@ -108,8 +120,8 @@ function Main() {
       </div>
       <div className="w-full grid grid-cols-4 gap-4 mt-11">
         {getSortedRecords().map((item, index) => (
-          <div 
-          onClick={() => handelOpenModalProduct(item)}
+          <div
+            onClick={() => handelOpenModalProduct(item)}
             key={index}
             className="w-full cursor-pointer h-full p-4 border border-gray-300 rounded-md shadow-md flex flex-col justify-center items-center"
           >
@@ -120,7 +132,10 @@ function Main() {
             />
             <p className="text-sm font-medium line-clamp-1">{item.title}</p>
             <p className="text-gray-700 mb-2">${item.price}</p>
-            <button className="bg-gradient-to-r from-white via-pink-500 to-red-500 p-2 rounded-md mb-2 shadow-black">
+            <button
+              onClick={() => handelOpenModalCart(item)}
+              className="bg-gradient-to-r from-white via-pink-500 to-red-500 p-2 rounded-md mb-2 shadow-black"
+            >
               Add to Cart
             </button>
           </div>
@@ -132,13 +147,56 @@ function Main() {
           <div className="w-full h-full">
             <div className="flex h-full justify-between gap-10">
               <div className="flex h-full items-center">
-              <img src={selectedProduct.image} alt={selectedProduct.title} className="h-72 object-cover mb-4" />
+                <img
+                  src={selectedProduct.image}
+                  alt={selectedProduct.title}
+                  className="h-72 object-cover mb-4"
+                />
               </div>
               <div className="me-10 w-2/4 h-full items-center justify-center flex flex-col">
-                <h1 className="text-4xl text-center font-bold line-clamp-1 mb-4">{selectedProduct.title}</h1>
-                <p className="text-center mb-4">{selectedProduct.description}</p>
-                <p className="text-center mb-4">Rating: {selectedProduct.rating.rate}</p>
-                <button className="w-full text-white font-semibold bg-gradient-to-r from-white via-pink-500 to-red-500 p-2 rounded-md mb-2 shadow-black">Add to Cart</button>
+                <h1 className="text-4xl text-center font-bold line-clamp-1 mb-4">
+                  {selectedProduct.title}
+                </h1>
+                <p className="text-center mb-4">
+                  {selectedProduct.description}
+                </p>
+                <p className="text-center mb-4">
+                  Rating: {selectedProduct.rating.rate}
+                </p>
+                <button className="w-full text-white font-semibold bg-gradient-to-r from-white via-pink-500 to-red-500 p-2 rounded-md mb-2 shadow-black">
+                  Add to Cart
+                </button>
+              </div>
+            </div>
+          </div>
+        </Modal>
+      )}
+
+      {modalCartOpen && selectedProductCart && (
+        <Modal onClose={handelCloseModal}>
+          {/* Content for the modal */}
+          <div className="w-full h-full">
+            <div className="flex h-full justify-between gap-10">
+              <div className="flex h-full items-center">
+                <img
+                  src={selectedProductCart.image}
+                  alt={selectedProductCart.title}
+                  className="h-72 object-cover mb-4"
+                />
+              </div>
+              <div className="me-10 w-2/4 h-full items-center justify-center flex flex-col">
+                <h1 className="text-4xl text-center font-bold line-clamp-1 mb-4">
+                  {selectedProductCart.title}
+                </h1>
+                <p className="text-center mb-4">
+                  {selectedProductCart.description}
+                </p>
+                <p className="text-center mb-4">
+                  Rating: {selectedProductCart.rating.rate}
+                </p>
+                <button className="w-full text-white font-semibold bg-gradient-to-r from-white via-pink-500 to-red-500 p-2 rounded-md mb-2 shadow-black">
+                  Add to Cart
+                </button>
               </div>
             </div>
           </div>
